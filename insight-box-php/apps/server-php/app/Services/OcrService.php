e9cbd299fc0b7e521aafa51bda35ea863816861a
Ryota Namiki <ryotanamiki@ip-192-168-32-21.ap-northeast-1.compute.internal>
2025-10-10 11:29:30 +0900

    specify full path to tesseract executable in OcrService

--- insight-box-php/apps/server-php/app/Services/OcrService.php
+++ insight-box-php/apps/server-php/app/Services/OcrService.php
@@ -8,11 +8,17 @@ class OcrService
 {
     public function imageToText(string $imagePath, string $lang = 'jpn+eng'): string
     {
-        return (new TesseractOCR($imagePath))
+        $ocr = (new TesseractOCR($imagePath))
             ->lang(...explode('+', $lang))
             ->oem(1)
-            ->psm(3)
-            ->run();
+            ->psm(3);
+        
+        // Tesseractのフルパスを指定（サーバー環境用）
+        if (file_exists('/usr/bin/tesseract')) {
+            $ocr->executable('/usr/bin/tesseract');
+        }
+        
+        return $ocr->run();
     }
 
     public function imageOrTextToText(string $path, string $lang): string
